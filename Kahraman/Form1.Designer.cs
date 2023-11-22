70d57ae874ce2fcaa2cc9b7fb2a7a45135ccfdf9
fenrirsoftware <fenrirsoftware@protonmail.com>
2023-11-22 11:13:40 +0300

    Proje dosyası ekle.

---
+++ Kahraman/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Kahraman
+{
+    partial class 窗体
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.按钮检查密码 = new System.Windows.Forms.Button();
+            this.文本框密码 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // 按钮检查密码
+            // 
+            this.按钮检查密码.Location = new System.Drawing.Point(61, 71);
+            this.按钮检查密码.Name = "按钮检查密码";
+            this.按钮检查密码.Size = new System.Drawing.Size(119, 52);
+            this.按钮检查密码.TabIndex = 1;
+            this.按钮检查密码.Text = "Login";
+            this.按钮检查密码.UseVisualStyleBackColor = true;
+            this.按钮检查密码.Click += new System.EventHandler(this.按钮检查密码_Click);
+            // 
+            // 文本框密码
+            // 
+            this.文本框密码.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.文本框密码.Location = new System.Drawing.Point(61, 25);
+            this.文本框密码.Name = "文本框密码";
+            this.文本框密码.Size = new System.Drawing.Size(119, 31);
+            this.文本框密码.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pass";
+            // 
+            // 窗体
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 135);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.文本框密码);
+            this.Controls.Add(this.按钮检查密码);
+            this.Name = "窗体";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button 按钮检查密码;
+        private System.Windows.Forms.TextBox 文本框密码;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Kahraman/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Kahraman
+
+{ //hint 
+    //maps kullan
+
+
+   
+    public partial class 窗体 : Form
+    {
+
+        private int 生成的密码;
+        string 分机 = "cat";
+
+
+        public 窗体()
+        {
+            InitializeComponent();
+            生成的密码 = 生成密码();
+          
+            
+            
+
+        }
+
+
+
+        private void 按钮检查密码_Click(object sender, EventArgs e)
+        {
+            string 输入的密码 = 文本框密码.Text;
+
+            int 输入的密码整数;
+            if (int.TryParse(输入的密码, out 输入的密码整数))
+            {
+                // 如果输入的值可以转换为整数，则进行比较
+                if (输入的密码整数 == 生成的密码)
+                {   
+                    MessageBox.Show("Şifre doğru!");
+
+
+                    string 文件路径 = "dosya.coderx37";
+                    string 新文件名 = Path.ChangeExtension(文件路径,"."+ 应用(分机));
+
+
+                    try
+                    {
+                        File.Move(文件路径, 新文件名);
+                        MessageBox.Show("Dosya uzantısı değiştirildi!");    
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Dosya uzantısı değiştirilemedi: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Şifre yanlış!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen sadece sayılardan oluşan bir şifre girin!");
+            }
+        }
+
+        private int 生成密码()
+        {
+            // 生成包含 5 位随机数字的密码
+            Random 随机数生成器 = new Random();
+            int 密码长度 = 5;
+            int 密码 = 0;
+
+            for (int i = 0; i < 密码长度; i++)
+            {
+                // 添加一个在 0 到 9 之间的随机数字
+                密码 = 密码 * 10 + 随机数生成器.Next(10);
+            }
+
+            return 密码;
+        }
+
+
+        static string 应用(string 输入)
+        {
+            char[] 字符数组 = 输入.ToCharArray();
+
+            for (int i = 0; i < 字符数组.Length; i++)
+            {
+                char 字符 = 字符数组[i];
+
+                if (字符 >= 'a' && 字符 <= 'z')
+                {
+                    字符数组[i] = (char)(((字符 - 'a' + 13) % 26) + 'a');
+                }
+                else if (字符 >= 'A' && 字符 <= 'Z')
+                {
+                    字符数组[i] = (char)(((字符 - 'A' + 13) % 26) + 'A');
+                }
+            }
+
+            return new string(字符数组);
+        }
+
+
+
+    }
+}
